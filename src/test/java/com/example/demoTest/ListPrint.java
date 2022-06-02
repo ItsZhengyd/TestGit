81dd9496318024eac8e1ec747b201a90489f9f5c
xiao_lo01@163.com <xiao_lo01@163.com>
2022-06-02 15:52:29 +0800

    for循环

---
+++ src/test/java/com/example/demoTest/ListPrint.java
@@ -0,0 +1,12 @@
+package com.example.demoTest;
+
+public class ListPrint {
+    public static void main(String[] args) {
+
+    }
+    public void ForToPrint(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
